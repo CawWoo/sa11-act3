3e50e5e825f9fa888d95fd32433780ae72dd535f
CawWoo <melodicraven@gmail.com>
2024-03-22 14:06:06 -0500

    Initial commit

---
+++ prob1.rb
@@ -0,0 +1,10 @@
+#Objective: Develop a Ruby program to extract all URLs from a block of text.
+#Your program should be able to recognize URLs that start with http, https, or ftp,
+#and could contain various domain names and paths.
+
+def extract_urls(text)
+end
+
+sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
+
+extract_urls(sample_text)
