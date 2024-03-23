99d9c00f808c11fbf390965dc3227190eff49429
CawWoo <melodicraven@gmail.com>
2024-03-22 19:56:13 -0500

    Solved problem 1

--- prob1.rb
+++ prob1.rb
@@ -3,6 +3,11 @@
 #and could contain various domain names and paths.
 
 def extract_urls(text)
+  extractor = %r{\b(?:http|https|ftp)://\S+\b}
+
+  text.scan(extractor) do |url|
+    puts url
+  end
 end
 
 sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
